b873819359de1c948c3bea57a0c5d98445b2c03f
Nigmat <harrynick005@gmail.com>
2024-04-08 20:10:52 -0400

    update

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -41,6 +38,33 @@ public class UserController {
 
         return "redirect:/user/create"; // redirecting to endpoint not view, if there is no redirect then will go to view
 
+    }
+
+    /* todo
+             path variable username will be defined in create.html when user clicks "Update" button, then pathVariable will
+             be present in http request and  @PathVariable(value = "username") will assign value to (String primaryKey_UserName)
+             then we can use primaryKey_UserName which is assigned userName from path variable to invoke UserDTO object that we want to update
+         */
+    @GetMapping("/update/{username}") // username is primary key (need to use primary key for the object you want to capture)
+    public String editUser(@PathVariable("username") String primaryKey_UserName, Model model){
+        UserDTO tobeUpdated=userService.findById(primaryKey_UserName);
+
+        model.addAttribute("userToBeUpdated",tobeUpdated);
+
+        // to fill Role dropDown
+        model.addAttribute("roles",roleService.findAll());
+
+        // to fill UserList table
+        model.addAttribute("users",userService.findAll());
+
+        return "/user/update";
+    }
+
+    @PostMapping("/update")
+    public String update( UserDTO userDTO) {
+        userService.update(userDTO);
+        System.out.println(userService.findById(userDTO.getUserName()));
+        return "redirect:/user/create";
     }
 
 
